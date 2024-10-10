5eca77b0f9d07b53850bd2d1e669e5d09dbe3272
VuHieu257 <vuhieu17062003@gmail.com>
2024-10-10 09:37:37 +0700

    bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:ui_youtex/core/size/size.dart';
 import 'package:ui_youtex/pages/screens/home/home.dart';
 import 'package:ui_youtex/pages/splash/Welcome/Register/RegisterScreen.dart';
 
 import 'package:ui_youtex/pages/splash/Welcome/welcome.dart';
+import 'package:ui_youtex/pages/widget_small/bottom_navigation/bottom_navigation.dart';
 
+import 'core/assets.dart';
 import 'core/themes/theme_data.dart';
 import 'pages/splash/Welcome/Register/login_screen.dart';
 
@@ -27,7 +30,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       // home: HomePage(),
-      home: WelcomeApp(),
+      home: const CustomNavBar(),
+      // home: WelcomeApp(),
       routes: {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
